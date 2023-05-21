9186412f040b729f205c194cfa43c6e3bb7cb8ac
Mahima de Silva <desilvamahima02@gmail.com>
2023-05-22 01:36:59 +0530

    added weather-api config file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 import cityList from '../assets/cities.json';
 import auth0_config from '../../auth-config.json';
+import weather_api_config from '../../weather-api-config.json';
 
 export const environment = {
   production: false,
-  WEATHER_API_KEY: 'e4e5a640da20f3a05005a29745eda68c',
+  WEATHER_API_KEY: weather_api_config.key,
   CITY_CODE_LIST: cityList.List.map((city) => city.CityCode),
-  API_ENDPOINT: `https://api.openweathermap.org/data/2.5/weather?id={%ID%}&appid={%APIKEY%}`,
+  API_ENDPOINT: weather_api_config.endpoint,
   AUTH0_DOMAIN: auth0_config.domain,
   AUTH0_CLIENTID: auth0_config.client_id,
   AUTH0_REDIRECT_URL: window.location.origin,
