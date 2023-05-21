e437a5eff1f1e8725a8250e9301c7d5e827ea485
Mahima de Silva <desilvamahima02@gmail.com>
2023-05-22 01:27:42 +0530

    changed enviroment.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 import cityList from '../assets/cities.json';
+import auth0_config from '../../auth-config.json';
 
 export const environment = {
-  production: true,
+  production: false,
   WEATHER_API_KEY: 'e4e5a640da20f3a05005a29745eda68c',
   CITY_CODE_LIST: cityList.List.map((city) => city.CityCode),
   API_ENDPOINT: `https://api.openweathermap.org/data/2.5/weather?id={%ID%}&appid={%APIKEY%}`,
-  AUTH0_DOMAIN: 'dev-k32hvagg1s6lxk07.us.auth0.com',
-  AUTH0_CLIENTID: 'CupIE0ojmTuMQpczUWY3cn84c2iQ3C0a',
-  AUTH0_REDIRECT_URL: window.location.origin
+  AUTH0_DOMAIN: auth0_config.domain,
+  AUTH0_CLIENTID: auth0_config.client_id,
+  AUTH0_REDIRECT_URL: window.location.origin,
 };
